b2ab80295b7419b28cfd616ec142ecf49a3a16c4
renbuairen <1664844124@qq.com>
2022-06-27 22:14:24 +0800

    全程结束

--- src/main.js
+++ src/main.js
@@ -8,3 +8,21 @@ import $ from 'jquery'
 // 处理css
 // import './styles/index.css';
 import './styles/index.less';
+
+import imgUrl from './assets/1.gif';
+let img = document.createElement('img');
+img.src = imgUrl;
+document.body.appendChild(img);
+
+import imgUrl1 from './assets/logo_small.png';
+let img1 = document.createElement('img');
+img1.src = imgUrl1;
+document.body.appendChild(img1);
+
+// 引入字体图标文件
+import './assets/fonts/iconfont.css'
+
+class App {
+    static a = 123
+}
+console.log(App.a)
--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,85 @@ module.exports = {
                 // 再用 style-loader 将样式, 把css插入到dom中
                 use: ['style-loader', 'css-loader', 'less-loader'],
             },
+            /* {
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                use: [
+                    {//webpack 4
+                        loader: 'url-loader', // 匹配文件, 尝试转base64字符串打包到js中
+                        // 配置limit, 超过8k, 不转, file-loader复制, 随机名, 输出文件
+                        options: {
+                            limit: 8 * 1024,
+                        },
+                    },
+                ],
+            } */
+            { // 图片文件的配置(仅适用于webpack5版本)
+                test: /\.(png|jpg|gif|jpeg)$/i,
+                // type: 'asset', // 在导出一个 data URI 和发送一个单独的文件之间自动选择
+                // 如果你设置的是asset模式
+                // 以8KB大小区分图片文件
+                // 小于8KB的, 把图片文件转base64, 打包进js中
+                // 大于8KB的, 直接把图片文件输出到dist下
+
+                // type: 'asset/resource' // 发送一个单独的文件并导出 URL
+                // type: 'asset/inline' // 导出一个资源的 data URI
+                type: 'asset',
+                parser: {//解析器 规则
+                    dataUrlCondition: {// datafrl的情况
+                        maxSize: 8 * 1024,
+                        // maxSize 限制最大值
+                    }
+                },
+                generator: {//生成器
+                    filename: '[hash:6][ext]'
+                }
+            },
+            { // webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'font-[name].[hash:6][ext]'
+                }
+            },
+            { // 处理字体图标的解析//webpack4
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1 * 1024,
+                            // 配置输出的文件名
+                            name: '[name].[ext]',
+                            // 配置输出的文件目录
+                            outputPath: "fonts/"
+                        }
+                    }
+                ]
+            },
+            // {webpack5默认内部不认识这些文件, 所以当做静态资源直接输出即可
+            //   test: /\.(eot|svg|ttf|woff|woff2)$/,//匹配所有的字体图标的文件
+            //   type: 'asset',//文件直接输出
+            //   generator: {//生产器
+            //     filename: 'font-[name].[hash:6][ext]'
+            //   },
+            //   parser: {// 解析器 规则
+            //     dataUrlCondition: {// dataUrl的h况
+            //       maxSize: 1 * 1024,
+            //       // maxSize 限制最大值、
+            //     }
+            //   }
+            // }
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/, // 排除在外 
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'] // 预设:转码规则(用bable开发环境本来预设的)
+                    }
+                }
+            },
+
         ]
     }
 }
