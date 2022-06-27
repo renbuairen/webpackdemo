08227edc96e7abb8f632280424b7f1df8e9d7fbc
renbuairen <1664844124@qq.com>
2022-06-27 20:46:58 +0800

    配置出入口自动生成html,清除内容

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path")
+// 引入自动生成 html 的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
     entry: "./src/main.js", // 入口
     output: {
         path: path.resolve(__dirname, "dist"), // 出口路径 绝对路径
         filename: "bundle.js" // 出口文件名
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // 告诉webpack使用插件时, 以我们自己的html文件作为模板去生成dist/html文件
+            filename: 'index.html' // 生成文件的名称
+        }),
+
+        new CleanWebpackPlugin(), // 删除的是ouput path 里配置的那个输出文件的文件夹
+    ]
 }
